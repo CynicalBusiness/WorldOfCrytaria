cc0e93b950e2ac898faf8969f53f37b3ebac6bc0
Matthew Struble <Matt@MayateckInnovations.net>
2013-12-01 11:27:25 -0800

    Created Plugin Main Class

--- MiscPlugin/src/net/mayateck/GameMisc/GameMisc.java
+++ MiscPlugin/src/net/mayateck/GameMisc/GameMisc.java
@@ -1,7 +1,72 @@
 package net.mayateck.GameMisc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GameMisc extends JavaPlugin{
-
+	public FileConfiguration pList = null;
+	public File pListFile = null;
+	public Plugin plugin = this;
+	
+	@Override
+	public void onEnable(){
+		DataEventHandler eh = new DataEventHandler(this);
+		getServer().getPluginManager().registerEvents(eh, this);
+		saveDefaultPList();
+		saveDefaultConfig();
+		getLogger().info("Enabled GameMisc data.");
+	}
+	
+	@Override
+	public void onDisable(){
+		
+		getLogger().info("Disabled GameMisc data.");
+	}
+	
+	public void reloadPList(){
+		if (pListFile==null){
+			pListFile = new File(getDataFolder(), "players.yml");
+		}
+		pList = YamlConfiguration.loadConfiguration(pListFile);
+		
+		InputStream defStream = this.getResource("players.yml");
+		if (defStream != null){
+			YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defStream);
+			pList.setDefaults(defConfig);
+		}
+	}
+	
+	public FileConfiguration getPList(){
+		if (pList==null){
+			reloadPList();
+		}
+		return pList;
+	}
+	
+	public void savePList(){
+		if (pList==null || pListFile==null){
+			return;
+		}
+		try {
+			getPList().save(pListFile);
+		} catch (IOException e) {
+			getLogger().log(Level.SEVERE, "Could not save config to " + pListFile, e);
+		}
+	}
+	
+	public void saveDefaultPList(){
+		if (pListFile == null){
+			pListFile = new File(getDataFolder(), "players.yml");
+		}
+		if (!pListFile.exists()){
+			plugin.saveResource("players.yml", false);
+		}
+	}
 }
