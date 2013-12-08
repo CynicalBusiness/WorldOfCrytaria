30bd0fc9d61c25fac0d72c78d44419ab5ac82ef1
Matthew Struble <Matt@MayateckInnovations.net>
2013-12-07 20:47:30 -0800

    Added CommandHandler execution trigger

--- CitiesPlugin/src/net/mayateck/GameCities/GameCities.java
+++ CitiesPlugin/src/net/mayateck/GameCities/GameCities.java
@@ -5,11 +5,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class GameCities extends JavaPlugin {
 	@Override
 	public void onEnable(){
+		CommandHandler cmdh = new CommandHandler(this);
+		getCommand("city").setExecutor(cmdh);
 		
+		getLogger().info("Enabled Cities.");
 	}
 	
 	@Override
 	public void onDisable(){
 		
+		getLogger().info("Disabled Cities");
 	}
 }
