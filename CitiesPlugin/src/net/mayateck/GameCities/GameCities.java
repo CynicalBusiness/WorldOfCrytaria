6a92d2bd292ab42260abdbeaa9e5808339120180
Matthew Struble <Matt@MayateckInnovations.net>
2013-12-07 20:16:47 -0800

    Cities Class Base

---
+++ CitiesPlugin/src/net/mayateck/GameCities/GameCities.java
@@ -0,0 +1,15 @@
+package net.mayateck.GameCities;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GameCities extends JavaPlugin {
+	@Override
+	public void onEnable(){
+		
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
+}
