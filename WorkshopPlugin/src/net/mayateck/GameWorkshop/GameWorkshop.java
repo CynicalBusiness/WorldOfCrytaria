8b15cdf14f162e66fea4b3c05c61c98f0d34cd68
Matthew Struble <Matt@MayateckInnovations.net>
2013-12-07 20:11:13 -0800

    Workshop Class Base (2)

--- WorkshopPlugin/src/net/mayateck/GameWorkshop/GameWorkshop.java
+++ WorkshopPlugin/src/net/mayateck/GameWorkshop/GameWorkshop.java
@@ -4,4 +4,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class GameWorkshop extends JavaPlugin {
 	
+	@Override
+	public void onEnable(){
+		
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
 }
