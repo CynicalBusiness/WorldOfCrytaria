71f3a7fb3981e24e4b4518607c5276dc4712d8d5
Matthew Struble <Matt@MayateckInnovations.net>
2013-12-08 21:02:54 -0800

    Added header data and configuration updates

--- CitiesPlugin/src/net/mayateck/GameCities/GameCities.java
+++ CitiesPlugin/src/net/mayateck/GameCities/GameCities.java
@@ -3,9 +3,15 @@ package net.mayateck.GameCities;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GameCities extends JavaPlugin {
+	public static final String head = "&8&l||============|[&eCrytarian Cities&8&l]|============||&r";
+	public static final String tag = "&8&l[&eWoC Cities&8&l]&r ";
+	public static String version = "";
+	
 	@Override
 	public void onEnable(){
 		CommandHandler cmdh = new CommandHandler(this);
+		version = this.getDescription().getVersion();
+		saveDefaultConfig();
 		getCommand("city").setExecutor(cmdh);
 		
 		getLogger().info("Enabled Cities.");
