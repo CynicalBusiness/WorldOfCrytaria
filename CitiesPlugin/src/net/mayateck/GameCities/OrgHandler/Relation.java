ad80d2999e51979568b17b60ec8334089bf5dc8a
Matthew Struble <Matt@MayateckInnovations.net>
2013-12-10 22:55:49 -0800

    Created the enum for organization relationships.

---
+++ CitiesPlugin/src/net/mayateck/GameCities/OrgHandler/Relation.java
@@ -0,0 +1,15 @@
+package net.mayateck.GameCities.OrgHandler;
+
+public enum Relation {
+	// Passive
+	NEUTRAL,
+	
+	// Aggressive
+	WARRING, HOSTILE, ANNOIED,
+	
+	// Peaceful
+	ALLIED, FRIENDLY, HAPPY,
+	
+	// MISC
+	NULL
+}
