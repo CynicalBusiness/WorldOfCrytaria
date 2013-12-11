1cc313ebc3d694e86d4267d42d004bdba09b1206
Matthew Struble <Matt@MayateckInnovations.net>
2013-12-10 22:55:11 -0800

    Created base DataFetch class.

--- CitiesPlugin/src/net/mayateck/GameCities/OrgHandler/DataFetch.java
+++ CitiesPlugin/src/net/mayateck/GameCities/OrgHandler/DataFetch.java
@@ -1,5 +1,11 @@
 package net.mayateck.GameCities.OrgHandler;
 
+import org.bukkit.plugin.Plugin;
+
 public class DataFetch {
+	Plugin plugin = null;
 	
+	public DataFetch(Plugin p){
+		plugin=p;
+	}
 }
