ddbe8950790410b522b274c890345543cfc6f85e
Matthew Struble <Matt@MayateckInnovations.net>
2013-12-10 22:55:04 -0800

    Added creator for DataFetch object.

--- CitiesPlugin/src/net/mayateck/GameCities/GameCities.java
+++ CitiesPlugin/src/net/mayateck/GameCities/GameCities.java
@@ -1,5 +1,7 @@
 package net.mayateck.GameCities;
 
+import net.mayateck.GameCities.OrgHandler.DataFetch;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GameCities extends JavaPlugin {
@@ -7,8 +9,11 @@ public class GameCities extends JavaPlugin {
 	public static final String tag = "&8&l[&eWoC Cities&8&l]&r ";
 	public static String version = "";
 	
+	public DataFetch fetcher = null;
+	
 	@Override
 	public void onEnable(){
+		fetcher = new DataFetch(this);
 		CommandHandler cmdh = new CommandHandler(this);
 		version = this.getDescription().getVersion();
 		saveDefaultConfig();
@@ -20,6 +25,7 @@ public class GameCities extends JavaPlugin {
 	@Override
 	public void onDisable(){
 		
+		saveConfig();
 		getLogger().info("Disabled Cities");
 	}
 }
