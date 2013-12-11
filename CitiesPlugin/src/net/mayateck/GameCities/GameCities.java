fc948235cb4c2d906ca977d0224f79af0c3b58a1
Matthew Struble <Matt@MayateckInnovations.net>
2013-12-11 07:26:03 -0800

    Changed the tagline to match new name.

--- CitiesPlugin/src/net/mayateck/GameCities/GameCities.java
+++ CitiesPlugin/src/net/mayateck/GameCities/GameCities.java
@@ -5,8 +5,8 @@ import net.mayateck.GameCities.OrgHandler.DataFetch;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GameCities extends JavaPlugin {
-	public static final String head = "&8&l||============|[&eCrytarian Cities&8&l]|============||&r";
-	public static final String tag = "&8&l[&eWoC Cities&8&l]&r ";
+	public static final String head = "&8&l||============|[&eCrytarian Organizations&8&l]|============||&r";
+	public static final String tag = "&8&l[&eWoC Orgs&8&l]&r ";
 	public static String version = "";
 	
 	public DataFetch fetcher = null;
