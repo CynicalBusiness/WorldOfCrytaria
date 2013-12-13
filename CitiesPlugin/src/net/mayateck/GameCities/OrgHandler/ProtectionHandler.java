38ba0eba9fed3d2ddff9194eee94951c79a17daa
Matthew Struble <Matt@MayateckInnovations.net>
2013-12-12 21:27:26 -0800

    Added BlockBreakEvent stuff.

--- CitiesPlugin/src/net/mayateck/GameCities/OrgHandler/ProtectionHandler.java
+++ CitiesPlugin/src/net/mayateck/GameCities/OrgHandler/ProtectionHandler.java
@@ -7,23 +7,29 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.logging.Level;
 
 import net.mayateck.GameCities.GameCities;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
 public class ProtectionHandler implements Listener {
 	public HashMap<String, String> protPlayers = new HashMap<String, String>();
 		// PlayerName, Group
+	public List<Player> overridePlayers = Arrays.asList();
+	
 	public HashMap<Block,String> blockData = new HashMap<Block,String>();
 		// PunchedBlock, FortificationString
 	public HashMap<String,Integer> fs = new HashMap<String,Integer>();
@@ -35,7 +41,7 @@ public class ProtectionHandler implements Listener {
 		saveBlockDataToDisk();
 		fs.put("STONE", 8);
 		fs.put("LEATHER", 16);
-		fs.put("IRON", 32);
+		fs.put("IRON_INGOT", 32);
 		fs.put("DIAMOND", 64);
 		fs.put("OBSIDIAN", 128);
 	}
@@ -75,7 +81,30 @@ public class ProtectionHandler implements Listener {
 	
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent e){
-		
+		Block b = e.getBlock();
+		Player p = e.getPlayer();
+		if (blockData.containsKey(b)){
+			String bData = blockData.get(b);
+			int dashLoc = bData.indexOf('-');
+			int pipeLoc = bData.indexOf('|');
+			int colonLoc = bData.indexOf(':');
+			if (overridePlayers.contains(p)){
+				Organization org = Organization.getOrganizationByPlayer(plugin.getConfig(), p.getName());
+				String orgname = bData.substring(colonLoc+1, dashLoc-1);
+				String mat = bData.substring(dashLoc+1,pipeLoc-1);
+				if (org.getName().equalsIgnoreCase(orgname)){
+					b.getWorld().dropItemNaturally(b.getLocation(), new ItemStack(Material.getMaterial(mat), 1));
+					return;
+				}
+			}
+			int fortNum = Integer.parseInt(bData.substring(pipeLoc+1));
+			fortNum--;
+			if (fortNum>0){
+				e.setCancelled(true);
+			} else {
+				blockData.remove(b);
+			}
+		}
 	}
 	
 	public void saveBlockDataToDisk(){
