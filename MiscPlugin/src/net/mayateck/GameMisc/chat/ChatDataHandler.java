4c129a69a3bb8416a88e3daa9430fc7f93f100c5
Matthew Struble <Matt@MayateckInnovations.net>
2013-12-26 15:21:56 -0800

    Fixed chatting.

--- MiscPlugin/src/net/mayateck/GameMisc/chat/ChatDataHandler.java
+++ MiscPlugin/src/net/mayateck/GameMisc/chat/ChatDataHandler.java
@@ -1,12 +1,15 @@
 package net.mayateck.GameMisc.chat;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.plugin.Plugin;
 
+@SuppressWarnings("deprecation")
 public class ChatDataHandler implements Listener{
 	Plugin plugin;
 	
@@ -18,18 +21,23 @@ public class ChatDataHandler implements Listener{
 		NONE, LIGHT, HEAVY
 	}
 	
-	@EventHandler
-	public void onPlayerChatted(AsyncPlayerChatEvent e){
+	@EventHandler(priority=EventPriority.LOWEST)
+	public void onPlayerChatted(PlayerChatEvent e){
 		Player plyr = e.getPlayer();
+		plugin.getLogger().info(plyr.getName()+": "+e.getMessage());
 		Player[] allPlayers = plugin.getServer().getOnlinePlayers();
+		plugin.getLogger().info("Trying to send to "+allPlayers.length+" players...");
 		String tag = "["+plyr.getName()+"]: ";
 		String msg = e.getMessage();
-		for (int i=0; i>allPlayers.length; i++){
+		plugin.getLogger().info("Composed "+tag+msg);
+		for (int i=0; i<allPlayers.length; i++){
 			Player p = allPlayers[i];
 			if (!p.getName().equalsIgnoreCase(plyr.getName())){
+				plugin.getLogger().info("Sent to "+p.getName());
 				Location pLoc = plyr.getLocation();
 				Location loc = p.getLocation();
 				double diff = Math.abs(pLoc.getX()-loc.getX()) + Math.abs(pLoc.getZ()-loc.getZ());
+				plugin.getLogger().info("Diff: "+diff);
 				if (diff<=800){
 					p.sendMessage(tag+msg);
 				} else if (diff<=1000){
@@ -38,6 +46,7 @@ public class ChatDataHandler implements Listener{
 					p.sendMessage(obfusicateString(tag, ObfChatLevel.HEAVY)+obfusicateString(msg, ObfChatLevel.HEAVY));
 				}
 			} else {
+				plugin.getLogger().info("Tried to send to self. No obfusication.");
 				p.sendMessage(tag+msg);
 			}
 		}
@@ -46,19 +55,22 @@ public class ChatDataHandler implements Listener{
 	
 	public String obfusicateString(String string, ObfChatLevel level){
 		int charRand = 0;
+		plugin.getLogger().info("Called "+level.toString()+".");
 		switch (level){
-			case LIGHT: charRand=10;
-			case HEAVY: charRand=30;
-			default: charRand=0;
+			case LIGHT: charRand=20; break;
+			case HEAVY: charRand=40; break;
+			case NONE: charRand=0; break;
 		}
-		char[] chars = string.toCharArray();
-		for (int i=0; i<chars.length; i++){
+		String newstr = "";
+		for (int i=0; i<string.length(); i++){
 			double randBreak = Math.ceil(Math.random()*100);
+			// plugin.getLogger().info(randBreak+"<=?"+charRand);
 			if (randBreak<=charRand){
-				chars[i]='*';
+				newstr += ChatColor.MAGIC+"*"+ChatColor.RESET;
+			} else {
+				newstr += string.charAt(i);
 			}
 		}
-		string=String.copyValueOf(chars);
-		return string;
+		return newstr;
 	}
 }
