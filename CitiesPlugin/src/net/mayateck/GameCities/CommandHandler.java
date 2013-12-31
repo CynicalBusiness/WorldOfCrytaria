a046694a292b9cab3b63e56770a10a21ed1e069b
Matthew Struble <Matt@MayateckInnovations.net>
2013-12-30 22:11:23 -0800

    Fixed some issues with incorrect strings.

--- CitiesPlugin/src/net/mayateck/GameCities/CommandHandler.java
+++ CitiesPlugin/src/net/mayateck/GameCities/CommandHandler.java
@@ -37,31 +37,43 @@ public class CommandHandler implements CommandExecutor{
 		case NULL:
 			plugin.getLogger().info("onCommand fired from "+s.getName()+" with null return.");
 			s.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"Woah, a &cprogramming error&r occurred. &oPlease let a dev know!&r"));
+			break;
 		case SUCCESS:
 			plugin.getLogger().info("onCommand fired from "+s.getName()+" with success.");
-			s.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"Command succeeded!"));
+			//s.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"Command succeeded!"));
+			break;
 		case BAD_SENDER:
 			s.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"Sorry, this command cannot be run from this sender."));
+			break;
 		case BAD_ARG_COUNT:
 			s.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"Bad argument count. Try &7/org help &ffor syntax"));
+			break;
 		case BAD_ARG:
 			s.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"Bad argument. Try &7/org help &ffor syntax."));
+			break;
 		case NO_PERMISSION:
 			s.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"Sorry, you don't have the ability to do this."));
+			break;
 		case ALREADY_EXISTS:
 			s.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"Sorry, something you tried to make already exists."));
+			break;
 		case MUST_LEAVE:
 			s.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"You can't do that while you're in an organization."));
+			break;
 		case NO_ORG_PERMISSION:
 			s.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"Sorry, your organization doesn't permit you to do that."));
+			break;
 		case BAD_ORG:
 			s.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"That organization doesn't exist."));
+			break;
 		case NOT_IN_ORG:
 			s.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"You're not in an organization!"));
+			break;
 		case TODO:
 			s.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"&9TODO&r."));
+			break;
 		}
-		return false;
+		return true;
 	}
 	
 	public CommandOutput parseCommand(CommandSender s, Command cmd, String label, String[] args){
@@ -91,7 +103,7 @@ public class CommandHandler implements CommandExecutor{
 								if (org.isReal==false){
 									String orgName = args[1].toLowerCase();
 									if (orgName.length()>plugin.getConfig().getInt("config.ORGNAME_MIN_LENGTH") && orgName.length()<plugin.getConfig().getInt("config.ORGNAME_MAX_LENGTH") && orgName.matches("^[A-Za-z_]{1,}$")){
-										if (!plugin.getConfig().contains("data.organizations."+orgName)){
+										if (!plugin.getConfig().contains("organizations."+orgName)){
 											plugin.getConfig().set("organizations."+orgName+".owner", p.getName());
 											plugin.getConfig().set("organizations."+orgName+".groups.admin.players", Arrays.asList(p.getName()));
 											plugin.getConfig().set("organizations."+orgName+".groups.admin.permissions", Arrays.asList("edit.*", "ranks.*", "econ.*", "research.*", "fortify.*"));
@@ -102,6 +114,7 @@ public class CommandHandler implements CommandExecutor{
 											plugin.getConfig().set("organizations."+orgName+".desc", "");
 											plugin.getConfig().set("organizations."+orgName+".funds", 0.0);
 											plugin.getConfig().set("organizations."+orgName+".relations", "");
+											plugin.saveConfig();
 											return CommandOutput.SUCCESS;
 										} else {
 											return CommandOutput.ALREADY_EXISTS;
@@ -127,7 +140,9 @@ public class CommandHandler implements CommandExecutor{
 						if (args[1].equalsIgnoreCase("tag")){
 							if (args.length==3){
 								List<String> perms = Arrays.asList("edit.tag", "edit.*", "edit.kitmod");
-								if (org.groupHasPermission(org.getPlayerGroup(p.getName()), perms, false)){
+								String pgroup = org.getPlayerGroup(p.getName());
+								plugin.getLogger().info("Player is in group "+pgroup);
+								if (org.groupHasPermission(pgroup, perms, false)){
 									if (args[2].length()>plugin.getConfig().getInt("config.ORGNAME_MIN_LENGTH") && args[2].length()<plugin.getConfig().getInt("config.ORGNAME_MAX_LENGTH") && args[2].matches("^[A-Za-z_]{1,}$")){
 										org.setTag(args[2]);
 										org.writeDataToDisk();
@@ -272,9 +287,8 @@ public class CommandHandler implements CommandExecutor{
 					return CommandOutput.BAD_ARG;
 				}
 			} else {
-				args[0]="help";
-				onCommand(s, cmd, label, args);
-				return CommandOutput.NULL;
+				String[] newarg = {"help"};
+				return parseCommand(s, cmd, label, newarg);
 			}
 		}
 		return CommandOutput.NULL;
