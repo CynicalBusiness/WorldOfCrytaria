1eb17af1d4c20dd450df9af940e9438c817be506
Matthew Struble <Matt@MayateckInnovations.net>
2014-03-09 09:24:42 -0700

    Repaired disk issues with foritifications.

--- CitiesPlugin/mayateck/GameCities/OrgHandler/ProtectionHandler.java
+++ CitiesPlugin/mayateck/GameCities/OrgHandler/ProtectionHandler.java
@@ -30,7 +30,7 @@ public class ProtectionHandler implements Listener {
 		// PlayerName, Group
 	public List<Player> overridePlayers = Arrays.asList();
 	
-	public HashMap<Block,String> blockData = new HashMap<Block,String>();
+	public HashMap<SerialLocation,String> blockData = new HashMap<SerialLocation, String>();;
 		// PunchedBlock, FortificationString
 	public HashMap<String,Integer> fs = new HashMap<String,Integer>();
 		// FortificationKey, ForitifcationValue
@@ -38,73 +38,108 @@ public class ProtectionHandler implements Listener {
 	
 	public ProtectionHandler(Plugin p){
 		plugin=p;
-		saveBlockDataToDisk();
 		fs.put("STONE", 8);
 		fs.put("LEATHER", 16);
-		fs.put("IRON_INGOT", 32);
-		fs.put("DIAMOND", 64);
-		fs.put("OBSIDIAN", 128);
+		fs.put("OBSIDIAN", 32);
+		fs.put("IRON_INGOT", 64);
+		fs.put("DIAMOND", 128);
+		fs.put("EMERALD", 256);
 	}
 	
 	@EventHandler
 	public void onPlayerClickEvent(PlayerInteractEvent e){
+		loadBlockDataFromDisk();
 		Player p = e.getPlayer();
 		Block b = e.getClickedBlock();
 		if (protPlayers.containsKey(p.getName())){
-			if (!blockData.containsKey(b)){
-				Organization org = Organization.getOrganizationByPlayer(plugin.getConfig(), p.getName());
-					// Organization has already been checked to ensure it exists.
+			boolean contains = false;
+			String fortInfo = "";
+			for (SerialLocation sloc : blockData.keySet()){
+				if (sloc.getLocation(plugin).getX() == b.getLocation().getX() && 
+						sloc.getLocation(plugin).getY() == b.getLocation().getY() &&
+						sloc.getLocation(plugin).getZ() == b.getLocation().getZ() &&
+						sloc.getLocation(plugin).getWorld().getName().equalsIgnoreCase(b.getLocation().getWorld().getName())){
+					contains=true;
+					fortInfo=blockData.get(sloc);
+					int dashLoc = fortInfo.indexOf('-');
+					int pipeLoc = fortInfo.indexOf('|');
+					int colonLoc = fortInfo.indexOf(':');
+					String group = fortInfo.substring(0, colonLoc);
+					String orgname = fortInfo.substring(colonLoc+1, dashLoc);
+					String mat = fortInfo.substring(dashLoc+1,pipeLoc);
+					int fortNum = Integer.parseInt(fortInfo.substring(pipeLoc+1));
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"This block is already fortified with "+mat+"(x"+fortNum+") by "+group+":"+orgname+"."));
+				}
+			}
+			if (!contains){
+				Organization org = Organization.getOrganizationByPlayer(plugin.getConfig(), p.getName(), plugin);
+				// Organization has already been checked to ensure it exists.
 				String imStr = e.getItem().getType().toString();
 				if (fs.containsKey(imStr)){
 					int fortVal = fs.get(imStr);
 					// Permissions and such are already checked.
-					String fortInfo = org.getPlayerGroup(p.getName())+":"+org.getName()+"-"+imStr+"|"+fortVal;
-					blockData.put(b,fortInfo);
+					fortInfo = org.getTag()+":"+org.getName()+"-"+imStr+"|"+fortVal;
+					SerialLocation nloc = new SerialLocation();
+					nloc.setLocation(b.getLocation());
+					blockData.put(nloc,fortInfo);
+					if (e.getItem().getAmount()<=1){
+						p.getInventory().setItemInHand(new ItemStack(Material.AIR));
+					} else {
+						e.getItem().setAmount(e.getItem().getAmount()-1);
+					}
 				} else {
-					p.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"The material "+e.getItem().getType().toString()+"is not valid for fortification."));
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"The material "+e.getItem().getType().toString()+" is not valid for fortification."));
 					protPlayers.remove(p);
 				}
-			} else {
-				String fortInfo = blockData.get(b);
-				int dashLoc = fortInfo.indexOf('-');
-				int pipeLoc = fortInfo.indexOf('|');
-				int colonLoc = fortInfo.indexOf(':');
-				String group = fortInfo.substring(0, colonLoc-1);
-				String orgname = fortInfo.substring(colonLoc+1, dashLoc-1);
-				String mat = fortInfo.substring(dashLoc+1,pipeLoc-1);
-				int fortNum = Integer.parseInt(fortInfo.substring(pipeLoc+1));
-				p.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"This block is already fortified with "+mat+"(x"+fortNum+") by "+group+":"+orgname+"."));
+				e.setCancelled(true);
 			}
-			e.setCancelled(true);
+			
+			saveBlockDataToDisk();
 		}
 	}
 	
+	
+	// TODO Fortification immunity from explosion/water/fire.
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent e){
+		loadBlockDataFromDisk();
 		Block b = e.getBlock();
 		Player p = e.getPlayer();
-		if (blockData.containsKey(b)){
-			String bData = blockData.get(b);
-			int dashLoc = bData.indexOf('-');
-			int pipeLoc = bData.indexOf('|');
-			int colonLoc = bData.indexOf(':');
-			if (overridePlayers.contains(p)){
-				Organization org = Organization.getOrganizationByPlayer(plugin.getConfig(), p.getName());
-				String orgname = bData.substring(colonLoc+1, dashLoc-1);
-				String mat = bData.substring(dashLoc+1,pipeLoc-1);
-				if (org.getName().equalsIgnoreCase(orgname)){
-					b.getWorld().dropItemNaturally(b.getLocation(), new ItemStack(Material.getMaterial(mat), 1));
-					return;
+		SerialLocation removalQue = null;
+		for (SerialLocation sloc : blockData.keySet()){
+			if (sloc.getLocation(plugin).getX() == b.getLocation().getX() && 
+					sloc.getLocation(plugin).getY() == b.getLocation().getY() &&
+					sloc.getLocation(plugin).getZ() == b.getLocation().getZ() &&
+					sloc.getLocation(plugin).getWorld().getName().equalsIgnoreCase(b.getLocation().getWorld().getName())){
+				String bData = blockData.get(sloc);
+				int dashLoc = bData.indexOf('-');
+				int pipeLoc = bData.indexOf('|');
+				int colonLoc = bData.indexOf(':');
+				if (overridePlayers.contains(p)){
+					Organization org = Organization.getOrganizationByPlayer(plugin.getConfig(), p.getName(), plugin);
+					String orgname = bData.substring(colonLoc+1, dashLoc-1);
+					String mat = bData.substring(dashLoc+1,pipeLoc-1);
+					if (org.getName().equalsIgnoreCase(orgname)){
+						Block usb = (Block) b;
+						usb.getWorld().dropItemNaturally(usb.getLocation(), new ItemStack(Material.getMaterial(mat), 1));
+						return;
+					}
+				}
+				int fortNum = Integer.parseInt(bData.substring(pipeLoc+1));
+				fortNum=fortNum-1;
+				if (fortNum>0){
+					String nonum = bData.substring(0, pipeLoc+1);
+					blockData.put(sloc, nonum+fortNum);
+					e.setCancelled(true);
+				} else {
+					removalQue=sloc;
 				}
 			}
-			int fortNum = Integer.parseInt(bData.substring(pipeLoc+1));
-			fortNum--;
-			if (fortNum>0){
-				e.setCancelled(true);
-			} else {
-				blockData.remove(b);
-			}
 		}
+		if (removalQue!=null){
+			blockData.remove(removalQue);
+		}
+		saveBlockDataToDisk();
 	}
 	
 	public void saveBlockDataToDisk(){
@@ -116,6 +151,7 @@ public class ProtectionHandler implements Listener {
 		} catch (FileNotFoundException e1) {
 			plugin.getLogger().log(Level.WARNING, "The blockdata file was missing. Attempting to create...");
 			new File(plugin.getDataFolder()+File.separator+"blocks.bin");
+			saveBlockDataToDisk();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -127,7 +163,7 @@ public class ProtectionHandler implements Listener {
 			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(plugin.getDataFolder()+File.separator+"blocks.bin"));
 			Object result = ois.readObject();
 			ois.close();
-			blockData = (HashMap<Block,String>)result;
+			blockData = (HashMap<SerialLocation,String>)result;
 		} catch (FileNotFoundException e1) { 
 			plugin.getLogger().log(Level.WARNING, "The blockdata file was missing. Calling data save...");
 			saveBlockDataToDisk();
