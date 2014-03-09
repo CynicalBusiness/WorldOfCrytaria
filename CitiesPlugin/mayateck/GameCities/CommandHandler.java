07c403d6ba712e96d4de4cc6e5849667ea2ee832
Matthew Struble <Matt@MayateckInnovations.net>
2014-03-09 09:24:05 -0700

    Added & fixed several ORG commands.

--- CitiesPlugin/mayateck/GameCities/CommandHandler.java
+++ CitiesPlugin/mayateck/GameCities/CommandHandler.java
@@ -2,6 +2,7 @@ package net.mayateck.GameCities;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 import net.mayateck.GameCities.OrgHandler.Organization;
 import net.mayateck.GameCities.OrgHandler.ProtectionHandler;
@@ -17,14 +18,18 @@ import org.bukkit.plugin.Plugin;
 public class CommandHandler implements CommandExecutor{
 	public enum CommandOutput{
 		SUCCESS, NULL, BAD_SENDER, BAD_ARG_COUNT, BAD_ARG,
-		NO_PERMISSION, ALREADY_EXISTS, MUST_LEAVE, NO_ORG_PERMISSION,
+		NO_PERMISSION, ALREADY_EXISTS, ALREADY_JOINED, MUST_LEAVE, NO_ORG_PERMISSION,
 		BAD_ORG, NOT_IN_ORG, TODO
 	}
 	
 	Plugin plugin = null;
 	ProtectionHandler ph = null;
 	List<String> helpList = Arrays.asList(
-			"&e/org create [name] &r- &7Founds a new organization.");
+			"&e/org create <name> &r- &7Founds a new organization.",
+			"&e/org disband &r- &7&cDisbands&7 your organization.",
+			"&e/org set <obj> [args] &r- &7Sets organization properties.",
+			"&e/org relations <cmd> [args] &r- &7Sets and views relation(s).",
+			"&e/org list [page] &r- &7Lists all organizations.");
 	
 	public CommandHandler(Plugin p, ProtectionHandler ph){
 		plugin=p;
@@ -56,6 +61,9 @@ public class CommandHandler implements CommandExecutor{
 			break;
 		case ALREADY_EXISTS:
 			s.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"Sorry, something you tried to make already exists."));
+			break;
+		case ALREADY_JOINED:
+			s.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"That player has already joined."));
 			break;
 		case MUST_LEAVE:
 			s.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"You can't do that while you're in an organization."));
@@ -95,25 +103,57 @@ public class CommandHandler implements CommandExecutor{
 					} else {
 						return CommandOutput.BAD_SENDER;
 					}
+				} else if (args[0].equalsIgnoreCase("list")){
+					if (args.length == 2){
+						int page = (Integer.parseInt(args[1])-1)*10;
+						int fpage = page+10;
+						Set<String> orgs = plugin.getConfig().getConfigurationSection("organizations").getKeys(false);
+						int pages = (int) Math.ceil(orgs.size()/10);
+						p.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.head));
+						p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Displaying page &e"+(page+1)+"&7 of &e"+(pages+1)+"&7."));
+						for (int i = page; i < fpage; i++){
+							if (orgs.size()<page){
+								p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7There are no organizations to display."));
+								return CommandOutput.SUCCESS;
+							} else {
+								String[] orgsa = orgs.toArray(new String[10]);
+								try {
+									String orgn = orgsa[i];
+									Organization org = new Organization(plugin.getConfig(), orgn);
+									if (org.isReal){
+										p.sendMessage(ChatColor.translateAlternateColorCodes('&', org.toString(true)));
+									}
+								} catch (ArrayIndexOutOfBoundsException e){
+									// Just trying to avoid an error throw.
+									plugin.getLogger().info("Reached end of bounds for array of organizations");
+									return CommandOutput.SUCCESS;
+								}
+							} 
+						} return CommandOutput.SUCCESS;
+					} else if(args.length == 1) {
+						String[] newargs = {"list", "1"};
+						return parseCommand(s,cmd,label,newargs);
+					} else {
+						return CommandOutput.BAD_ARG_COUNT;
+					}
 				} else if (args[0].equalsIgnoreCase("create") || args[0].equalsIgnoreCase("new")){
 					if (isPlayer){
 						if (args.length==2){
 							if (s.hasPermission("organizations.create")){
-								Organization org = Organization.getOrganizationByPlayer(plugin.getConfig(), p.getName());
+								Organization org = Organization.getOrganizationByPlayer(plugin.getConfig(), p.getName(), plugin);
 								if (org.isReal==false){
 									String orgName = args[1].toLowerCase();
 									if (orgName.length()>plugin.getConfig().getInt("config.ORGNAME_MIN_LENGTH") && orgName.length()<plugin.getConfig().getInt("config.ORGNAME_MAX_LENGTH") && orgName.matches("^[A-Za-z_]{1,}$")){
 										if (!plugin.getConfig().contains("organizations."+orgName)){
 											plugin.getConfig().set("organizations."+orgName+".owner", p.getName());
 											plugin.getConfig().set("organizations."+orgName+".groups.admin.players", Arrays.asList(p.getName()));
-											plugin.getConfig().set("organizations."+orgName+".groups.admin.permissions", Arrays.asList("edit.*", "ranks.*", "econ.*", "research.*", "fortify.*"));
 											plugin.getConfig().set("organizations."+orgName+".groups.moderator.players", Arrays.asList());
-											plugin.getConfig().set("organizations."+orgName+".groups.moderator.permissions", Arrays.asList("edit.kitmod", "ranks.kitmod", "econ.kitmod", "research.*", "fortify.kitmod"));
-											plugin.getConfig().set("organizations."+orgName+".groups.default.permissions", Arrays.asList("econ.kitcitizen", "research.kitcitizen", "fortify.kitcitizen"));
 											plugin.getConfig().set("organizations."+orgName+".tag", orgName);
 											plugin.getConfig().set("organizations."+orgName+".desc", "");
 											plugin.getConfig().set("organizations."+orgName+".funds", 0.0);
 											plugin.getConfig().set("organizations."+orgName+".relations", "");
+											plugin.getConfig().set("organizations."+orgName+".invited", Arrays.asList());
+											plugin.getConfig().set("organizations."+orgName+".players", Arrays.asList(p.getName()));
 											plugin.saveConfig();
 											return CommandOutput.SUCCESS;
 										} else {
@@ -136,16 +176,15 @@ public class CommandHandler implements CommandExecutor{
 					}
 				} else if (args[0].equalsIgnoreCase("set")){
 					if (args.length>1){
-						Organization org = Organization.getOrganizationByPlayer(plugin.getConfig(), p.getName());
+						Organization org = Organization.getOrganizationByPlayer(plugin.getConfig(), p.getName(), plugin);
 						if (args[1].equalsIgnoreCase("tag")){
 							if (args.length==3){
-								List<String> perms = Arrays.asList("edit.tag", "edit.*", "edit.kitmod");
 								String pgroup = org.getPlayerGroup(p.getName());
 								plugin.getLogger().info("Player is in group "+pgroup);
-								if (org.groupHasPermission(pgroup, perms, false)){
+								if (org.isAdmin(p.getName()) || org.isMod(p.getName())){
 									if (args[2].length()>plugin.getConfig().getInt("config.ORGNAME_MIN_LENGTH") && args[2].length()<plugin.getConfig().getInt("config.ORGNAME_MAX_LENGTH") && args[2].matches("^[A-Za-z_]{1,}$")){
 										org.setTag(args[2]);
-										org.writeDataToDisk();
+										org.writeDataToDisk(org.getName());
 										plugin.saveConfig();
 										return CommandOutput.SUCCESS;
 									} else {
@@ -159,19 +198,18 @@ public class CommandHandler implements CommandExecutor{
 							}
 						} else if (args[1].equalsIgnoreCase("desc")){
 							if (args.length > 2){
-								List<String> perms = Arrays.asList("edit.desc", "edit.*", "edit.kitmod");
-								if (org.groupHasPermission(org.getPlayerGroup(p.getName()), perms, false)){
+								if (org.isAdmin(p.getName()) || org.isMod(p.getName())){
 									String desc = "";
 									int i = 1;
 									for (String arg : args){
 										if (i>2){
-											desc+=arg;
+											desc+=arg+" ";
 										}
 										i++;
 									}
 									desc.replaceAll("'", "''");
 									org.setDesc(desc);
-									org.writeDataToDisk();
+									org.writeDataToDisk(org.getName());
 									plugin.saveConfig();
 									return CommandOutput.SUCCESS;
 								} else {
@@ -187,7 +225,7 @@ public class CommandHandler implements CommandExecutor{
 						return CommandOutput.BAD_ARG_COUNT;
 					}
 				} else if (args[0].equalsIgnoreCase("relations")){
-					Organization org = Organization.getOrganizationByPlayer(plugin.getConfig(), p.getName());
+					Organization org = Organization.getOrganizationByPlayer(plugin.getConfig(), p.getName(), plugin);
 					if (org.isReal){
 						if (args[1].equalsIgnoreCase("view")){
 							if (args.length==4){
@@ -243,8 +281,7 @@ public class CommandHandler implements CommandExecutor{
 						return CommandOutput.NOT_IN_ORG;
 					}
 				} else if (args[0].equalsIgnoreCase("fortify")) {
-					Organization org = Organization.getOrganizationByPlayer(plugin.getConfig(), p.getName());
-					List<String> perms = Arrays.asList("fortify.*", "fortify.kitcitizen", "fortify.kitmod", "fortify.add");
+					Organization org = Organization.getOrganizationByPlayer(plugin.getConfig(), p.getName(), plugin);
 					if (org.isReal){
 						if (args.length<3){
 							String group = "";
@@ -255,18 +292,12 @@ public class CommandHandler implements CommandExecutor{
 									return CommandOutput.BAD_ARG;
 								}
 							} else {
-								group = org.getPlayerGroup(p.getName());
+								group = org.getName();
 							}
-							if (org.groupHasPermission(group, perms, false)){
-								if (ph.protPlayers.containsKey(p)){
-									if (ph.protPlayers.get(p).equalsIgnoreCase(group)){
-										ph.protPlayers.remove(p);
-										p.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"You are no longer in fortify mode."));
-									} else {
-										ph.protPlayers.remove(p);
-										ph.protPlayers.put(p.getName(), group.toLowerCase());
-										p.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"Changed foritification group to '"+group+"'."));
-									}
+							if (org.isAdmin(p.getName()) || org.isMod(p.getName())){
+								if (ph.protPlayers.containsKey(p.getName())){
+									ph.protPlayers.remove(p.getName());
+									p.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"You are no longer in fortify mode."));
 								} else {
 									ph.protPlayers.put(p.getName(), group.toLowerCase());
 									p.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"Now fortifying for group '"+group+"'."));
@@ -281,8 +312,50 @@ public class CommandHandler implements CommandExecutor{
 					} else {
 						return CommandOutput.NOT_IN_ORG;
 					}
-				} else if (args[0].equalsIgnoreCase("delete")) {
-					
+				} else if (args[0].equalsIgnoreCase("delete") || args[0].equalsIgnoreCase("disband")) {
+					Organization org = Organization.getOrganizationByPlayer(plugin.getConfig(), p.getName(), plugin);
+					if (org.isReal){
+						if (org.isOwner(p.getName())){
+							plugin.getServer().broadcastMessage(ChatColor.translateAlternateColorCodes('&', "&eThe organization of "+org.getTag()+"("+org.getName()+") was disbanded."));
+							String oname = org.getName();
+							plugin.getConfig().set("organizations."+oname, null);
+							plugin.saveConfig();
+							return CommandOutput.SUCCESS;
+						} else {
+							return CommandOutput.NO_ORG_PERMISSION;
+						}
+					} else {
+						return CommandOutput.NOT_IN_ORG;
+					}
+				} else if (args[0].equalsIgnoreCase("invite")){
+					if (args.length==2){
+						Organization org = Organization.getOrganizationByPlayer(plugin.getConfig(), p.getName(), plugin);
+						if (org.isReal){
+							if (org.isAdmin(p.getName()) || org.isMod(p.getName())){
+								String inv = args[1];
+								if (!org.getPlayers().contains(inv) && !org.getInvited().contains(inv)){
+									org.addInvited(inv);
+									p.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"Successfully invited &e"+inv+"&7."));
+									return CommandOutput.SUCCESS;
+								} else {
+									if (org.getInvited().contains(inv)){
+										org.delInvited(inv);
+										p.sendMessage(ChatColor.translateAlternateColorCodes('&', GameCities.tag+"Revoked &e"+inv+"&7's invite."));
+										return CommandOutput.SUCCESS;
+									}
+										return CommandOutput.ALREADY_JOINED;
+								}
+							} else {
+								return CommandOutput.NO_PERMISSION;
+							}
+						} else {
+							return CommandOutput.NOT_IN_ORG;
+						}
+					} else {
+						return CommandOutput.BAD_ARG_COUNT;
+					}
+				} else if (args[0].equalsIgnoreCase("join")){
+					return CommandOutput.TODO;
 				} else {
 					return CommandOutput.BAD_ARG;
 				}
