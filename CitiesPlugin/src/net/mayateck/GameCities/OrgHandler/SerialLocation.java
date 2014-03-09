9d12eb434cd9ec1f4ad4c28078d969d189fc7b5c
Matthew Struble <Matt@MayateckInnovations.net>
2014-03-09 09:32:06 -0700

    Reorganized to solve some issues with Eclispe.

--- CitiesPlugin/src/net/mayateck/GameCities/OrgHandler/SerialLocation.java
+++ CitiesPlugin/src/net/mayateck/GameCities/OrgHandler/SerialLocation.java
@@ -6,21 +6,21 @@ import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.plugin.Plugin;
 
-public class SerialLocation implements Serializable{
+public class SerialLocation implements Serializable {
 	private static final long serialVersionUID = 4200472796408840628L;
-	private double x,y,z;
+	private double x, y, z;
 	private String world;
-	
-	public void setLocation(Location loc){
-		x=loc.getX();
-		y=loc.getY();
-		z=loc.getZ();
-		world=loc.getWorld().getName();
-	}
-	
-	public Location getLocation(Plugin plugin){
+
+	public Location getLocation(Plugin plugin) {
 		World nworld = plugin.getServer().getWorld(world);
-		return new Location(nworld,x,y,z);
+		return new Location(nworld, x, y, z);
+	}
+
+	public void setLocation(Location loc) {
+		x = loc.getX();
+		y = loc.getY();
+		z = loc.getZ();
+		world = loc.getWorld().getName();
 	}
 
 }
