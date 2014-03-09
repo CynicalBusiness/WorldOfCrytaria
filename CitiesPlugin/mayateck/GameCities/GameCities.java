1c3a9988beb81ba39c85bf4b1d70ed2bd2a9b72a
Matthew Struble <Matt@MayateckInnovations.net>
2014-03-09 09:24:18 -0700

    Changed some variables.

--- CitiesPlugin/mayateck/GameCities/GameCities.java
+++ CitiesPlugin/mayateck/GameCities/GameCities.java
@@ -1,5 +1,7 @@
 package net.mayateck.GameCities;
 
+import java.util.logging.Logger;
+
 import net.mayateck.GameCities.OrgHandler.DataFetch;
 import net.mayateck.GameCities.OrgHandler.ProtectionHandler;
 
@@ -7,8 +9,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class GameCities extends JavaPlugin {
 	public static final String head = "&8&l||============|[&eCrytarian Organizations&8&l]|============||&r";
-	public static final String tag = "&8&l[&eWoC Orgs&8&l]&r ";
+	public static final String tag = "&8&l[&eWoC Orgs&8&l]&7 ";
 	public static String version = "";
+	public static Logger logger;
 	
 	public DataFetch fetcher = null;
 	public ProtectionHandler protEventHandler = null;
@@ -27,6 +30,7 @@ public class GameCities extends JavaPlugin {
 		
 		this.getConfig().addDefaults(this.getConfig());
 		
+		logger = getLogger();
 		getLogger().info("Enabled WoC.Organizations.");
 	}
 	
